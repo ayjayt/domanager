62c4edba3fb191b3d0caba759a8103697d8978ac
Andrew Pikul <ajpikul@gmail.com>
2019-07-22 23:23:12 +0000

    Add basic DNS lookup for all domain names

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module domanager
 
 go 1.12
 
-require github.com/spf13/viper v1.4.0
+require (
+	github.com/domainr/whois v0.0.0-20180714175948-975c7833b02e
+	github.com/likexian/whois-go v0.0.0-20190627090909-384b3df3fc49
+	github.com/likexian/whois-parser-go v0.0.0-20190429170710-0aa0498833b2
+	github.com/registrobr/rdap v0.0.0-20180924175303-5dbb0c1b6b69
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/spf13/viper v1.4.0
+	github.com/zonedb/zonedb v1.0.2369 // indirect
+)
--- main.go
+++ main.go
@@ -2,38 +2,89 @@
 Domanager reads a YAML file like:
 ```
 domains:
-	- username:password
-	- username:password
+	-
+	-
 ```
 
-and logs into name cheap, pulls the domains and where they point to, all records for them, and then tries to see what they're SSL records are like.
+and pulls the domains info and where they point to, all records for them, and then tries to see what they're SSL records are like.
 */
 package main
 
 import (
 	"fmt"
-
 	"github.com/spf13/viper"
-	// configuration
-	// namecheap
+	"net"
+	"net/http"
+	"time"
 	// letsencrypt
 	// logging
 )
 
+const ()
+
 func init() {
+	var err error
 	viper.SetConfigName("domains")
 	viper.AddConfigPath("/etc/domanager")
 	viper.AddConfigPath("$HOME/")
 	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
+	err = viper.ReadInConfig()
 	if err != nil {
 		panic(err)
 	}
 }
 func main() {
-	list := viper.AllSettings()
+	list := viper.GetStringSlice("domains")
+	c := new(http.Client)
+	c.Timeout = time.Millisecond * 500
 	for i, v := range list {
-		fmt.Printf("%+v, %+v\n", i, v)
+		fmt.Printf("%+v, %+v:\n", i, v)
+
+		ips, err := net.LookupIP(v)
+		if err != nil {
+			fmt.Printf("\terr: %v\n", err)
+		} else {
+			fmt.Printf("\tIP: %v\n", ips)
+		}
+		txt, err := net.LookupTXT(v)
+		if err != nil {
+			fmt.Printf("\terr: %v\n", err)
+		} else {
+			fmt.Printf("\tTXT: %v\n", txt)
+		}
+		mx, err := net.LookupMX(v)
+		if err != nil {
+			fmt.Printf("\terr: %v\n", err)
+		} else {
+			fmt.Printf("\tMX: ")
+			for i, v := range mx {
+				if i == 0 {
+					fmt.Printf("%v, ", v.Host)
+				}
+			}
+			fmt.Printf("\n")
+		}
+		ns, err := net.LookupNS(v)
+		if err != nil {
+			fmt.Printf("\terr: %v\n", err)
+		} else {
+			fmt.Printf("\tNS: ")
+			for i, v := range ns {
+				if i == 0 {
+					fmt.Printf("%v", v.Host)
+				}
+			}
+			fmt.Printf("\n")
+		}
+		fmt.Printf("\tGET: ")
+		resp, err := c.Get("http://" + v)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		} else {
+			fmt.Printf("%v\n", resp.Status)
+			resp.Body.Close()
+		}
+
 	}
 	// read a configuration file for domains and keys
 	// get their records
